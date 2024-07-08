29cfb5b5a0aac579311aa0f74e7e61b1ac1f29ab
HigorMenz <higormenzinger1997@outlook.com>
2024-07-08 17:53:37 -0400

    bg color change on scrollY (MOBILE)

--- js/main.js
+++ js/main.js
@@ -13,16 +13,27 @@ if (navToggle) {
 
 //Menu Hidden
 if (navClose) {
-    navClose.addEventListener("click", () => {
-       navMenu.classList.remove("show-menu");
-    });
-  }
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 //remove mobile menu
 
-const navLink = document.querySelector(".nav__link")
+const navLinks = document.querySelectorAll(".nav__link");
 
-
-function linkAction(){
-    navMenu.classList.remove("show-menu")
+function linkAction() {
+    
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLinks.forEach(n => n.addEventListener("click", linkAction));
+
+//change header background
+
+function ScrollHeader() {
+    const navMenu = document.querySelector("#nav-menu");
+  const header = document.querySelector("header");
+
+  if (this.scrollY >= 50) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+  
+}
+window.addEventListener("scroll", ScrollHeader);
